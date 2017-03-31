6e844ca19df5e2af57564b36f5cdf71e8ff2de9d
durow <durow@163.com>
2017-03-31 10:59:02 +0800

    learn how to use menu

--- src/components/LayoutTest.vue
+++ src/components/LayoutTest.vue
@@ -4,9 +4,141 @@
             <div class="col-md-11">
                 <h4>栅格布局测试</h4> 主要学习测试bootstrap栅格布局
             </div>
-            <div class="input-group col-md-1">
+            <div class="col-md-1">
                 <button class="btn btn-info btn-lg"
                         v-on:click="changeLayout">切换布局</button>
+            </div>
+        </div>
+        <div class="btn-group margin">
+            <div class="btn-group">
+                <button class="btn btn-default dropdown-toggle"
+                        data-toggle="dropdown"
+                        id="test-button-1">
+                    TestButton1 <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu"
+                    aria-labelledby="test-button-1">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator"
+                        class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+            </div>
+            <div class="btn-group">
+                <button class="btn btn-default  dropdown-toggle"
+                        data-toggle="dropdown"
+                        id="test-button-2">
+                    TestButton1 <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu"
+                    aria-labelledby="test-button-2">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator"
+                        class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+            </div>
+            <div class="btn-group">
+                <button class="btn btn-default dropdown-toggle"
+                        type="button"
+                        id="dropdownMenu1"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="true">
+                    Dropdown
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu"
+                    aria-labelledby="dropdownMenu1">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator"
+                        class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+            </div>
+            <div class="btn-group">
+                <a href="#"
+                   class="btn btn-default">Link1</a>
+                <a href="#"
+                   class="btn btn-default">Link2</a>
+                <a href="#"
+                   class="btn btn-default">Link3</a>
+            </div>
+        </div>
+        <div class="btn-toolbar margin">
+            <div class="btn-group">
+                <button class="btn btn-default">1-1</button>
+                <button class="btn btn-default">1-2</button>
+                <button class="btn btn-default">1-3</button>
+            </div>
+            <div class="btn-group">
+                <button class="btn btn-default">2-1</button>
+                <button class="btn btn-default">2-2</button>
+            </div>
+            <div class="btn-group">
+                <button class="btn btn-default">3-1</button>
+                <button class="btn btn-default">3-2</button>
+                <button class="btn btn-default">3-3</button>
+            </div>
+        </div>
+        <div class="margin">
+            <div class="btn-group">
+                <button class="btn btn-default dropdown-toggle"
+                        data-toggle="dropdown"
+                        id="test-button-1">
+                    TestButton1 <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu"
+                    aria-labelledby="test-button-1">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator"
+                        class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+            </div>
+            <div class="btn-group">
+                <button class="btn btn-default">DropDown</button>
+                <button class="btn btn-default  dropdown-toggle"
+                        data-toggle="dropdown"
+                        id="test-button-2"><span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu"
+                    aria-labelledby="test-button-2">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator"
+                        class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+            </div>
+            <div class="btn-group">
+                <button class="btn btn-default dropdown-toggle"
+                        type="button"
+                        id="dropdownMenu1"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="true">
+                    Dropdown
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu"
+                    aria-labelledby="dropdownMenu1">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator"
+                        class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
             </div>
         </div>
         <div :class="layout">
